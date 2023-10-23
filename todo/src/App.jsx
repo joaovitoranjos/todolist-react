1b4e45d41470dc93abc971cb98f2fb55a6d15781
joaovitoranjos <joaoalmeids00@gmail.com>
2023-10-23 19:24:06 -0300

    funcionalidade dos botoes stats e asc/desc

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -68,7 +68,7 @@ function App() {
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch} />
-      <Filter filter={filter} setFilter={setFilter} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
         {todos
           .filter((todo) =>
@@ -81,6 +81,11 @@ function App() {
           .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLowerCase())
           )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
+          )
           .map((todo) => (
             <Todo
               key={todo.id}
@@ -89,7 +94,6 @@ function App() {
               completeTodo={completeTodo}
             />
           ))}
-        ;
       </div>
       <TodoForm addTodo={addTodo} />
     </div>
