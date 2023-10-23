67dac6c10cc6419d162bc356095960af1fced09a
joaovitoranjos <joaoalmeids00@gmail.com>
2023-10-23 19:10:05 -0300

    funcionalidade do botao filtro

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,95 +1,99 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import Todo from './components/Todo';
-import Search from './components/Search';
-import TodoForm from './components/TodoForm';
+import Todo from "./components/Todo";
+import Search from "./components/Search";
+import TodoForm from "./components/TodoForm";
 
 import "./App.css";
-import Filter from './components/Filter';
-
-
+import Filter from "./components/Filter";
 
 function App() {
   const [todos, setTodos] = useState([
     {
       id: 1,
-      text:"Criar funcionalidade x no sistema",
+      text: "Criar funcionalidade x no sistema",
       category: "Trabalho",
       isCompleted: false,
-
     },
     {
       id: 2,
-      text:"Ir para a academia",
+      text: "Ir para a academia",
       category: "Pessoal",
       isCompleted: false,
-
     },
     {
       id: 3,
-      text:"Estudar React",
+      text: "Estudar React",
       category: "Estudos",
       isCompleted: false,
-
     },
-    
   ]);
 
   const [search, setSearch] = useState("");
 
-  const addTodo = (text, category) => {
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
+  const addTodo = (text, category) => {
     const newTodos = [
-      ...todos,  
+      ...todos,
       {
-      id: Math.floor(Math.random() * 10000),
-      text,
-      category,
-      isCompleted: false,
-    },
-  ];
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
 
     setTodos(newTodos);
   };
 
   const removeTodo = (id) => {
-    const newTodos = [...todos]
-    const filteredTodos = newTodos.filter((todo) => 
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter((todo) =>
       todo.id !== id ? todo : null
-      );
-      setTodos(filteredTodos);
+    );
+    setTodos(filteredTodos);
   };
 
   const completeTodo = (id) => {
-    const newTodos = [...todos]
+    const newTodos = [...todos];
     newTodos.map((todo) =>
-      todo.id === id ? (todo.isCompleted =!todo.isCompleted) : todo
-      ); 
-      setTodos(newTodos);
-  }
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
+    );
+    setTodos(newTodos);
+  };
 
   return (
-    <div className="app">  
+    <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch} />
-      <Filter />
-        <div className="todo-list">
-          {todos
+      <Filter filter={filter} setFilter={setFilter} />
+      <div className="todo-list">
+        {todos
+          .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
           .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLowerCase())
           )
           .map((todo) => (
-            <Todo 
+            <Todo
               key={todo.id}
               todo={todo}
-              removeTodo= {removeTodo}
+              removeTodo={removeTodo}
               completeTodo={completeTodo}
             />
-      ))}
+          ))}
+        ;
+      </div>
+      <TodoForm addTodo={addTodo} />
     </div>
-    <TodoForm addTodo={addTodo} />
-  </div> 
-  
-)}
+  );
+}
 
 export default App;
--- todo/src/components/Filter.jsx
+++ todo/src/components/Filter.jsx
@@ -1,22 +1,22 @@
-const Filter = () => {
+const Filter = ({ filter, setFilter }) => {
   return (
     <div className="filter">
-        <h2>Filtrar:</h2>
-        <div className="filter-options"> 
-            <div>
-                <p>Status:</p>
-                <select>
-                    <option value="All">Todas</option>
-                    <option value="Completed">Completas</option>
-                    <option value="Incomplete">Incompletas</option>
-                </select>
-            </div>
-            <div>
-                <p>Ordem alfabética</p>
-                <button>Asc</button>
-                <button>Desc</button>
-            </div>
-        </div >
+      <h2>Filtrar:</h2>
+      <div className="filter-options">
+        <div>
+          <p>Status:</p>
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="All">Todas</option>
+            <option value="Completed">Completas</option>
+            <option value="Incomplete">Incompletas</option>
+          </select>
+        </div>
+        <div>
+          <p>Ordem alfabética</p>
+          <button>Asc</button>
+          <button>Desc</button>
+        </div>
+      </div>
     </div>
   );
 };
